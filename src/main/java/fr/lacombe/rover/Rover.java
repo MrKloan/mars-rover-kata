3e451a0a971864af7f804882e823350e1e90dbbf
Valentin Fries <valentin.fries@lacombedulionvert.fr>
2018-10-23 11:44:26 +0200

    Rover that can turn

---
+++ src/main/java/fr/lacombe/rover/Rover.java
@@ -0,0 +1,39 @@
+package fr.lacombe.rover;
+
+import java.util.Objects;
+
+import static fr.lacombe.rover.Orientation.*;
+
+class Rover {
+
+    private final Orientation orientation;
+
+    Rover() {
+        this(NORTH);
+    }
+
+    Rover(final Orientation orientation) {
+        this.orientation = orientation;
+    }
+
+    Rover turnRight() {
+        return new Rover(orientation.right());
+    }
+
+    Rover turnLeft() {
+        return new Rover(orientation.left());
+    }
+
+    @Override
+    public boolean equals(final Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        final Rover rover = (Rover) o;
+        return orientation == rover.orientation;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(orientation);
+    }
+}
---
+++ src/test/java/fr/lacombe/rover/RoverTest.java
@@ -0,0 +1,28 @@
+package fr.lacombe.rover;
+
+import org.junit.Test;
+
+import static fr.lacombe.rover.Orientation.EAST;
+import static fr.lacombe.rover.Orientation.WEST;
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class RoverTest {
+
+    @Test
+    public void should_turn_right() {
+        final Rover rover = new Rover();
+
+        final Rover result = rover.turnRight();
+
+        assertThat(result).isEqualTo(new Rover(EAST));
+    }
+
+    @Test
+    public void should_turn_left() {
+        final Rover rover = new Rover();
+
+        final Rover result = rover.turnLeft();
+
+        assertThat(result).isEqualTo(new Rover(WEST));
+    }
+}
